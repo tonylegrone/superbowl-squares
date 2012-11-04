8b7df57f158d578ad4060481ccb728c7db74e609
Tony Legrone <tony@microfitgroup.com>
2012-11-03 19:18:26 -0500

    Added json2 library and base HTML template.

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<!--[if lt IE 9]><html class="ie"><![endif]-->
+<!--[if gte IE 9]><!--><html><!--<![endif]-->
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+  <title>Superbowl Squares</title>
+
+  <link rel="shortcut icon" href="/favicon.png" type="image/vnd.microsoft.icon" />
+  <link rel="stylesheet" href="/libraries/jquery-ui-1.9.1/css/ui-lightness/jquery-ui-1.9.1.custom.css" />
+  <link rel="stylesheet" href="/styles/global.css" type="text/css" />
+ 
+  <!--[if lt IE 9]>
+  <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+
+</head>
+<body>
+  <div id="main-wrapper">
+
+    <header>
+
+    </header>
+
+    <div id="page-wrapper"><section>
+      <div id="content"><section>
+        
+      </section></div><!-- /#content -->
+    </section></div><!-- /#page-wrapper -->
+
+    <footer>
+      
+    </footer>
+
+  </div><!-- /#main-wrapper -->
+
+  <script type="text/javascript" src="/libraries/jquery-ui-1.9.1/js/jquery-1.8.2.js"></script>
+  <script type="text/javascript" src="/libraries/jquery-ui-1.9.1/js/jquery-ui-1.9.1.custom.min.js"></script>
+  <script type="text/javascript" src="/libraries/json2/json2.js"></script>
+  <script type="text/javascript" src="/scripts/global.js"></script>
+</body>
+</html>
